f7be16d6361ab1eb6be32a7759bf60a630b2b35e
KZProgRus2023 <122629875+KZProgRus2023@users.noreply.github.com>
2023-05-21 10:29:34 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace TextEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.fildFilePath = new System.Windows.Forms.CheckedListBox();
+            this.butSelectFile = new System.Windows.Forms.Button();
+            this.butOpenFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numFont = new System.Windows.Forms.NumericUpDown();
+            this.fileContent = new System.Windows.Forms.TextBox();
+            this.butSaveFile = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.numFont)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите файл";
+            // 
+            // fildFilePath
+            // 
+            this.fildFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fildFilePath.FormattingEnabled = true;
+            this.fildFilePath.Location = new System.Drawing.Point(91, 13);
+            this.fildFilePath.Name = "fildFilePath";
+            this.fildFilePath.Size = new System.Drawing.Size(298, 19);
+            this.fildFilePath.TabIndex = 1;
+            // 
+            // butSelectFile
+            // 
+            this.butSelectFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butSelectFile.Location = new System.Drawing.Point(395, 12);
+            this.butSelectFile.Name = "butSelectFile";
+            this.butSelectFile.Size = new System.Drawing.Size(85, 20);
+            this.butSelectFile.TabIndex = 2;
+            this.butSelectFile.Text = "Выбрать";
+            this.butSelectFile.UseVisualStyleBackColor = true;
+            // 
+            // butOpenFile
+            // 
+            this.butOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.butOpenFile.Location = new System.Drawing.Point(486, 11);
+            this.butOpenFile.Name = "butOpenFile";
+            this.butOpenFile.Size = new System.Drawing.Size(75, 21);
+            this.butOpenFile.TabIndex = 3;
+            this.butOpenFile.Text = "Открыть";
+            this.butOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Шрифт";
+            // 
+            // numFont
+            // 
+            this.numFont.Location = new System.Drawing.Point(91, 47);
+            this.numFont.Maximum = new decimal(new int[] {
+            72,
+            0,
+            0,
+            0});
+            this.numFont.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numFont.Name = "numFont";
+            this.numFont.Size = new System.Drawing.Size(120, 20);
+            this.numFont.TabIndex = 5;
+            this.numFont.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            // 
+            // fileContent
+            // 
+            this.fileContent.Location = new System.Drawing.Point(2, 73);
+            this.fileContent.Multiline = true;
+            this.fileContent.Name = "fileContent";
+            this.fileContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.fileContent.Size = new System.Drawing.Size(587, 284);
+            this.fileContent.TabIndex = 6;
+            // 
+            // butSaveFile
+            // 
+            this.butSaveFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.butSaveFile.Location = new System.Drawing.Point(486, 364);
+            this.butSaveFile.Name = "butSaveFile";
+            this.butSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.butSaveFile.TabIndex = 7;
+            this.butSaveFile.Text = "Сохранить";
+            this.butSaveFile.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.butSaveFile.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 388);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(590, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(130, 17);
+            this.toolStripStatusLabel1.Text = "Количество символов";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 410);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.butSaveFile);
+            this.Controls.Add(this.fileContent);
+            this.Controls.Add(this.numFont);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.butOpenFile);
+            this.Controls.Add(this.butSelectFile);
+            this.Controls.Add(this.fildFilePath);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Текстовый редактор";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numFont)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox fildFilePath;
+        private System.Windows.Forms.Button butSelectFile;
+        private System.Windows.Forms.Button butOpenFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numFont;
+        private System.Windows.Forms.TextBox fileContent;
+        private System.Windows.Forms.Button butSaveFile;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public interface MainForm
+    {
+        string FilePath { get; }
+        string Content { get; set; }
+        void SetSymbolCount(int count);
+        event EventHandler FileOpenClick;
+        event EventHandler FileSaveClick;
+        event EventHandler ContentChanged;
+    }
+    public partial class MainForm : Form, IMainForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            butOpenFile.Click += new EventHandler(ButOpenFile_Click);
+            butSaveFile.Click += ButSaveFile_Click;
+            fldContent.TextChanged += fldContent_TextChanged;
+            butSelectFile.Click += ButSelectFile_Click;
+        }
+
+        private void ButSelectFile_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ButOpenFile_Click(object sender, EventArgs e)
+        {
+            if (FileOpenClick != null) FileOpenClick(this, EventArgs.Empty);
+        }
+        private void ButSaveFile_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #region Проброс событий
+        void butOpenFile.Click(object sender, EventArgs e)
+        {
+            if (FileOpenClick != null) FileOpenClick(this, EventArgs.Empty);
+        }
+
+        private void ButOpenFile_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IMainForm
+        public string FilePath
+        {
+            get{return fldFilePath.Text;}
+            set { fldContent.Text = value;}
+        }
+        public void SetSymbolCount(int count)
+        {
+            lblSymbolCount.Text = count.ToString();
+        }
+        public event EventHandler FileOpenClick;
+        public event EventHandler FileSaveClick;
+        public event EventHandler ContentChanged;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void numFont_ValueChanged(object sender, EventArgs e)
+        {
+            fldContent.Font=new Font("Calibri", (float)numFont.Value);
+        }
+    }
+}
